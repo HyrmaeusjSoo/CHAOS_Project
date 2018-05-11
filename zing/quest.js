a1a50f6a0707a65eddbc34b9511e612e35b0ff8d
Hylea Soo <1782284076@qq.com>
2018-05-11 16:43:10 +0800

    THB
    thing ing plan B

--- zing/quest.js
+++ zing/quest.js
@@ -1,2 +1,11 @@
-if that was javascript code?
-no.
+/*if that was javascript code?
+no.*/
+
+function _questOBH_A(){
+    //Good code style
+}
+
+function _questOBH_B()
+{
+    //Oh no, that fucking terrible shit bad code style plan.
+}
