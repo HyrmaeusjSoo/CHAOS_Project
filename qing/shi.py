819be28809e596f61f00c71cd2d2d125c083dec6
Hylea Soo <1782284076@qq.com>
2019-03-16 08:31:20 +0800

    己亥丁卯壬子甲辰
    己亥年丁卯月壬子日甲辰时

---
+++ qing/shi.py
@@ -0,0 +1,25 @@
+package together.
+
+  the. sun.
+
+  and. the. summer.
+
+  that. moon.
+
+  and. all. stars.
+
+  and. those. cool. crazy. cars.
+
+  and. the. sky.
+
+  its. amazing.
+
+  and. all. my. kids. laughing. waving.
+
+  call. it. life.
+
+  oh. i. wish.
+
+  it. just. werent. 
+
+  a. lie;
