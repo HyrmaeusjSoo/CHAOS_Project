bf29027b47a425ecabe788f8c557adcefe8fcac8
Hylea Soo <1782284076@qq.com>
2019-04-10 15:04:03 +0800

    己亥戊辰丁丑戊申
    己亥年戊辰月丁丑日戊申时

---
+++ xing/PianDuan.js
@@ -0,0 +1,67 @@
+// +---------------------------------------------------------------------------------------+
+// | Hylea Soo's from past to present 在りし日のふたり Void Chaos False Philosophy code.   |
+// +---------------------------------------------------------------------------------------+
+// | _wabbajack Numidium Human Requests. Hermaeus Mora's Oghma Infinium. We Are Observer.  |
+// +---------------------------------------------------------------------------------------+
+// | Copyright © 2019 Powered by HyleaSoo All Rights Reserved.                             |
+// +---------------------------------------------------------------------------------------+
+// |                                                            —————— 龍が如く Hylea Soo  |
+// +---------------------------------------------------------------------------------------+
+
+
+(() => {
+	
+	http://HyleaSoo.club
+	
+	1  .2  .3  .123. ;//3/4 1/3 1/6
+	123.123.123.123. ;//3/4
+	123.223.123.223. ;
+	
+	qyjlxn  ln
+
+	//  236.82    444.61
+	//  336.82    344.61
+	//  436.82    244.61
+	
+	367
+	
+})();
+
+(() => {
+	var goAndRunSeasonRift = function () {
+		console.clear();
+
+		let jiding = "17";
+		let fixed = (new Date((new Date()).toLocaleDateString() + ' '+jiding+':00:00')).getTime();
+		let now = (new Date()).getTime();
+
+		let milliseconds = fixed - now;
+		let seconds = (milliseconds / 1000).toString();
+		let minutes = (seconds / 60).toString();
+		let hours = (minutes / 60).toString();
+
+		console.log(hours);
+		console.log(minutes);
+		console.log(seconds);
+		console.log('--------------------');
+
+		let H = hours.substr(0, hours.indexOf(".") + 1);
+		let Hms = '0' + hours.substr(hours.indexOf("."));
+		let Hmc = (Hms * 6 / 10).toString().substr(2);
+		let Hm = Hmc.substr(0, 2);
+		let Hss = '0.' + Hmc.substr(2);
+		let Hsc = (Hss * 6 / 10).toString().substr(2);
+		let Hs = Hsc.substr(0, 2);
+		console.log(H + Hm + '.' + Hs);
+
+		let M = minutes.substr(0, minutes.indexOf(".") + 1);
+		let Mss = '0' + minutes.substr(minutes.indexOf("."));
+		let Msc = (Mss * 6 / 10).toString().substr(2);
+		let Ms = Msc.substr(0, 2);
+		console.log(M + Ms);
+
+		let S = seconds.substr(0, seconds.indexOf("."));
+		console.log(S);
+	};
+	setInterval(goAndRunSeasonRift, 2000);
+})();
