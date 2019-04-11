8ecf0f5d9d2be83a15200621b26c6758f5359bfa
Hylea Soo <1782284076@qq.com>
2019-04-11 09:02:14 +0800

    己亥戊辰戊寅丁巳
    己亥年戊辰月戊寅日丁巳时

--- xing/PianDuan.js
+++ xing/PianDuan.js
@@ -1,5 +1,5 @@
 // +---------------------------------------------------------------------------------------+
-// | Hylea Soo's from past to present 在りし日のふたり Void Chaos False Philosophy code.     |
+// | Hylea Soo's from past to present 在りし日のふたり Void Chaos False Philosophy code.      |
 // +---------------------------------------------------------------------------------------+
 // | _wabbajack Numidium Human Requests. Hermaeus Mora's Oghma Infinium. We Are Observer.  |
 // +---------------------------------------------------------------------------------------+
@@ -33,36 +33,36 @@
     var goAndRunSeasonRift = function () {
         console.clear();
 
-        let jiding = "17";
-        let fixed = (new Date((new Date()).toLocaleDateString() + ' '+jiding+':00:00')).getTime();
-        let now = (new Date()).getTime();
+        const dogo = 24 - 8 + 1;
+        const fixed = (new Date((new Date()).toLocaleDateString() + ' '+dogo+':00:00')).getTime();
+        const now = (new Date()).getTime();
 
-        let milliseconds = fixed - now;
-        let seconds = (milliseconds / 1000).toString();
-        let minutes = (seconds / 60).toString();
-        let hours = (minutes / 60).toString();
+        const milliseconds = fixed - now;
+        const seconds = (milliseconds / 1000).toString();
+        const minutes = (seconds / 60).toString();
+        const hours = (minutes / 60).toString();
 
         console.log(hours);
         console.log(minutes);
         console.log(seconds);
         console.log('--------------------');
 
-        let H = hours.substr(0, hours.indexOf(".") + 1);
+        const H = hours.substr(0, hours.indexOf(".") + 1);
         let Hms = '0' + hours.substr(hours.indexOf("."));
         let Hmc = (Hms * 6 / 10).toString().substr(2);
-        let Hm = Hmc.substr(0, 2);
+        const Hm = Hmc.substr(0, 2);
         let Hss = '0.' + Hmc.substr(2);
         let Hsc = (Hss * 6 / 10).toString().substr(2);
-        let Hs = Hsc.substr(0, 2);
+        const Hs = Hsc.substr(0, 2);
         console.log(H + Hm + '.' + Hs);
 
-        let M = minutes.substr(0, minutes.indexOf(".") + 1);
+        const M = minutes.substr(0, minutes.indexOf(".") + 1);
         let Mss = '0' + minutes.substr(minutes.indexOf("."));
         let Msc = (Mss * 6 / 10).toString().substr(2);
-        let Ms = Msc.substr(0, 2);
+        const Ms = Msc.substr(0, 2);
         console.log(M + Ms);
 
-        let S = seconds.substr(0, seconds.indexOf("."));
+        const S = seconds.substr(0, seconds.indexOf("."));
         console.log(S);
     };
     setInterval(goAndRunSeasonRift, 2000);
