8cafc6f89f31ae7d51773e5a3b187bf747664b21
Hylea Soo <1782284076@qq.com>
2020-12-10 16:52:24 +0800

    庚子丁亥丁亥戊申
    庚子年丁亥月丁亥日戊申时

--- ding/mhgu_query.sql
+++ ding/mhgu_query.sql
@@ -1,37 +1,37 @@
 
 -- base query katana range
-select i._id,w.parent_id,i.name,w.attack,w.element,w.element_attack,w.affinity,w.sharpness,w.num_slots,w.defense,
+SELECT i._id,w.parent_id,i.name,w.attack,w.element,w.element_attack,w.affinity,w.sharpness,w.num_slots,w.defense,
     w.creation_cost,w.upgrade_cost,w.wtype,w.final,w.family,i.rarity,i.description -- i.sub_type,
-from items i
-left join weapons w on i._id=w._id
-where i._id >= 524545 and i._id <= 556291; -- and wtype = 'Long Sword'
+FROM items i
+LEFT JOIN weapons w ON i._id=w._id
+WHERE i._id >= 524545 AND i._id <= 556291; -- AND wtype = 'Long Sword'
 
 -- max atc
-select i._id,w.parent_id,i.name,w.attack,w.element,w.element_attack,w.affinity,w.sharpness,w.num_slots,w.defense,
+SELECT i._id,w.parent_id,i.name,w.attack,w.element,w.element_attack,w.affinity,w.sharpness,w.num_slots,w.defense,
     w.creation_cost,w.upgrade_cost,w.wtype,w.final,w.family,i.rarity,i.description -- i.sub_type,
-from items i
-left join weapons w on i._id=w._id
-where i._id >= 524545 and i._id <= 556291
-order by attack desc;
+FROM items i
+LEFT JOIN weapons w ON i._id=w._id
+WHERE i._id >= 524545 AND i._id <= 556291
+ORDER BY attack DESC;
 
 -- max affinity
-select i._id,w.parent_id,i.name,w.attack,w.element,w.element_attack,w.affinity,w.sharpness,w.num_slots,w.defense,
+SELECT i._id,w.parent_id,i.name,w.attack,w.element,w.element_attack,w.affinity,w.sharpness,w.num_slots,w.defense,
     w.creation_cost,w.upgrade_cost,w.wtype,w.final,w.family,i.rarity,i.description -- i.sub_type,
-from items i
-left join weapons w on i._id=w._id
-where i._id >= 524545 and i._id <= 556291
-order by affinity*1 desc;
+FROM items i
+LEFT JOIN weapons w ON i._id=w._id
+WHERE i._id >= 524545 AND i._id <= 556291
+ORDER BY affinity*1 DESC;
 
 -- max final
-select i._id,w.parent_id,i.name,w.attack,(attack+15)*affinity*1.39 as atc,substr(w.sharpness,1,instr(w.sharpness,' ')-1) as sharpness,
+SELECT i._id,w.parent_id,i.name,w.attack,(attack+15)*affinity*1.39 AS atc,SUBSTR(w.sharpness,1,INSTR(w.sharpness,' ')-1) AS sharpness,
     w.affinity,w.element,w.element_attack,w.num_slots,i.rarity,w.defense,
     w.creation_cost,w.upgrade_cost,w.wtype,w.final,w.family,i.description,w.sharpness -- i.sub_type,
-from items i
-left join weapons w on i._id=w._id
-where i._id >= 524545 and i._id <= 556291 and w.final=1
+FROM items i
+LEFT JOIN weapons w ON i._id=w._id
+WHERE i._id >= 524545 AND i._id <= 556291 AND w.final=1
 
 
-select substr('6.4.5.16.5.4.0 6.4.5.16.5.6.0 6.4.5.16.5.9.0',1,instr('6.4.5.16.5.4.0 6.4.5.16.5.6.0 6.4.5.16.5.9.0',' ')-1)
+SELECT SUBSTR('6.4.5.16.5.4.0 6.4.5.16.5.6.0 6.4.5.16.5.9.0',1,INSTR('6.4.5.16.5.4.0 6.4.5.16.5.6.0 6.4.5.16.5.9.0',' ')-1)
 
 
 
